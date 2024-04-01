24fed0ef3245c803023c16cf9000990e4f6f532e
CrispyCandy <crispycandy750@gmail.com>
2024-04-01 15:33:49 +0800

    implements the test of SLList.getLast() operations

--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,5 +1,6 @@
 package timingtest;
 import edu.princeton.cs.algs4.Stopwatch;
+import net.sf.saxon.om.Item;
 
 /**
  * Created by hug.
@@ -22,7 +23,35 @@ public class TimeSLList {
     }
 
     public static void timeGetLast() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        for (int i = 0, N = 1000; i < 8; i++, N *= 2) {
+            runWithDataSizeOf(N, Ns, times, opCounts);
+        }
+
+        printTimingTable(Ns, times, opCounts);
     }
 
+    /**
+     * Run with data size of n and populate the Ns, times and opCounts.
+     */
+    private static void runWithDataSizeOf(int N, AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        SLList<Integer> list = new SLList<>();
+        for (int i = 0; i < N; i++) {
+            list.addFirst(i);
+        }
+
+        int opCount = 0;
+        Stopwatch stopwatch = new Stopwatch();
+        for (int i = 0; i < N; i++) {
+            list.getLast();
+            opCount++;
+        }
+        double timeInSeconds = stopwatch.elapsedTime();
+        Ns.addLast(N);
+        times.addLast(timeInSeconds);
+        opCounts.addLast(opCount);
+    }
 }

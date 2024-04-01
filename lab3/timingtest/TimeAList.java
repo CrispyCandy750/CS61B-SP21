80de940290186920327b5cb6cf5f514beb8a8128
CrispyCandy <crispycandy750@gmail.com>
2024-04-01 15:07:24 +0800

    add the runWithDataSizeOf() method and fill the code in timeAListConstruction()
    - runWithDataSizeOf(n, Ns, times, opCounts) method run n times add operations and populate the Ns, times, opCounts list.
    - timeAListConstruction() call the runWithDataSizeOf() multiples times to test the efficiency of addLast() operation

--- lab3/timingtest/TimeAList.java
+++ lab3/timingtest/TimeAList.java
@@ -22,6 +22,34 @@ public class TimeAList {
     }
 
     public static void timeAListConstruction() {
-        // TODO: YOUR CODE HERE
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        for (int n = 0, N = 1000; n < 8; n++, N *= 2) {
+            runWithDataSizeOf(N, Ns, times, opCounts);
+        }
+
+        printTimingTable(Ns, times, opCounts);
+    }
+
+    /**
+     * Run with data size of n and populate the Ns, times and opCounts.
+     */
+    private static void runWithDataSizeOf(int n, AList<Integer> Ns, AList<Double> times, AList<Integer> opCounts) {
+        AList<Integer> list = new AList<>();
+        Stopwatch stopwatch = new Stopwatch();
+        int opCount = 0;
+        
+        for (int i = 0; i < n; i++) {
+            list.addLast(i);
+            opCount++;
+        }
+
+        double timeInSeconds = stopwatch.elapsedTime();
+
+        Ns.addLast(n);
+        times.addLast(timeInSeconds);
+        opCounts.addLast(opCount);
     }
 }

f3dd72c7fd5dc6333cc1278ca99d7d4395b80759
CrispyCandy <crispycandy750@gmail.com>
2024-04-04 21:05:04 +0800

    complete the addFirst() method

--- proj1/deque/ArrayDeque.java
+++ proj1/deque/ArrayDeque.java
@@ -1,16 +1,39 @@
 package deque;
 
 public class ArrayDeque<T> implements Deque<T> {
+    private T[] deque;
+
+    /* first always points the first item. */
+    private int first;
+
+    /* last always points the successor of the last item. */
+    private int rear;
+
+    private static final int INITIAL_SIZE = 8;
+    private static final double MIN_USAGE_FACTOR = 0.25;
+    private static final double EXPAND_FACTOR = 1.25;
+    private static final double SHRINK_FACTOR = 0.75;
+
+    @SuppressWarnings("unchecked")
+    public ArrayDeque() {
+        deque = (T[]) new Object[INITIAL_SIZE];
+        first = rear = 0;
+    }
 
     /** Adds an item of type T to the front of the deque. */
     @Override
     public void addFirst(T item) {
-
+        first = prev(deque, first);
+        addToArr(deque, first, item);
+        if (isFull()) {
+            resize((int) (deque.length * EXPAND_FACTOR));
+        }
     }
 
     /** Adds an item of type T to the back of the deque. */
     @Override
     public void addLast(T item) {
+        // 1. addToArr  2. isFull  3. resize()
 
     }
 
@@ -64,4 +87,28 @@ public class ArrayDeque<T> implements Deque<T> {
 
     /* --------------------------- private class & methods --------------------------- */
 
+    /** Returns true if the deque is full, false otherwise. */
+    private boolean isFull() {
+        return next(deque, rear) == first;
+    }
+
+    private boolean resize(int newSize) {
+        return false;
+    }
+
+    /* --------------------------- abstraction barrier --------------------------- */
+    /** Returns the previous index of the param index. */
+    private int prev(T[] deque, int index) {
+        return (index - 1 + deque.length) % deque.length;
+    }
+
+    /** Returns the next index of the param index */
+    private int next(T[] deque, int index) {
+        return (index + 1) % deque.length;
+    }
+
+    /** Add before the index-th item. */
+    private void addToArr(T[] deque, int index, T item) {
+        deque[index] = item;
+    }
 }

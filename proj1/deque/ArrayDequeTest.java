401ef899eb46e693712dc690d5b130d97df25e23
CrispyCandy <crispycandy750@gmail.com>
2024-04-04 21:14:37 +0800

    test the isEmpty() method

--- proj1/deque/ArrayDequeTest.java
+++ proj1/deque/ArrayDequeTest.java
@@ -1,6 +1,7 @@
 package deque;
 
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class ArrayDequeTest {
 
@@ -8,5 +9,23 @@ public class ArrayDequeTest {
     public void addFirst() {
         ArrayDeque<String> deque = new ArrayDeque<>();
         deque.addFirst("abc");
+    }
+
+    @Test
+    public void addLast() {
+        ArrayDeque<String> deque = new ArrayDeque<>();
+        deque.addLast("abc");
+    }
+
+    @Test
+    public void isEmpty() {
+        ArrayDeque<String> deque = new ArrayDeque<>();
+
+        boolean condition1 = deque.isEmpty();
+        assertTrue(condition1);
+
+        deque.addFirst("abc");
+        boolean condition2 = deque.isEmpty();
+        assertFalse(condition2);
     }
 }

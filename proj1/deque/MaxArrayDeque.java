3214573de80d107edfbd04371a07eee6e348e4db
CrispyCandy <crispycandy750@gmail.com>
2024-04-05 15:16:23 +0800

    remove the MaxDeque interface

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -3,7 +3,7 @@ package deque;
 import java.util.Comparator;
 import java.util.Iterator;
 
-public class MaxArrayDeque<T> extends ArrayDeque<T> implements MaxDeque<T> {
+public class MaxArrayDeque<T> extends ArrayDeque<T> {
 
     private Comparator<T> comparator;
 
@@ -11,7 +11,10 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> implements MaxDeque<T> {
         this.comparator = comparator;
     }
 
-    @Override
+    /**
+     * returns the maximum element in the deque as governed by the previously given Comparator in constructor.
+     * If the MaxArrayDeque is empty, simply return null.
+     */
     public T max() {
         return max(this.comparator);
     }
@@ -21,7 +24,6 @@ public class MaxArrayDeque<T> extends ArrayDeque<T> implements MaxDeque<T> {
      * returns the maximum element in the deque as governed by the parameter Comparator c.
      * If the MaxArrayDeque is empty, simply return null.
      */
-    @Override
     public T max(Comparator<T> c) {
         if (this.isEmpty()) {
             return null;

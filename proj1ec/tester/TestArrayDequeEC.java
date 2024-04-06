054da1cf8b480e65b2ab309c690c1403e478d651
CrispyCandy <crispycandy750@gmail.com>
2024-04-06 11:14:48 +0800

    add the assert message
    - the current test print the operation sequence when assertion is wrong.

--- proj1ec/tester/TestArrayDequeEC.java
+++ proj1ec/tester/TestArrayDequeEC.java
@@ -3,38 +3,61 @@ package tester;
 import edu.princeton.cs.algs4.StdRandom;
 import org.junit.Test;
 import student.StudentArrayDeque;
+
 import static org.junit.Assert.*;
 
 /** Test the probably buggy StudentArrayDeque with correct ArrayDeque Solution */
 public class TestArrayDequeEC {
 
     @Test
-    public void randomizedTest () {
+    public void randomizedTest() {
         StudentArrayDeque<Integer> buggyDeque = new StudentArrayDeque<>();
         ArrayDequeSolution<Integer> correctDeque = new ArrayDequeSolution<>();
+
+        int operationNumber = 0;
+
+        StringBuilder operationSequence = new StringBuilder();
+        String operation = "";
+
         int N = 10000;
 
         for (int i = 0; i < N; i++) {
-            int operationNumber = StdRandom.uniform(0, 4);
+            operationNumber = StdRandom.uniform(0, 4);
             switch (operationNumber) {
                 case 0:  // addFirst
                     int addNumber = StdRandom.uniform(0, 100);
                     buggyDeque.addFirst(addNumber);
                     correctDeque.addFirst(addNumber);
+
+                    operation = "addFirst(" + addNumber + ")";
+                    operationSequence.append(operation + "\n");
+
                     break;
                 case 1:  // addLast
                     addNumber = StdRandom.uniform(0, 100);
                     buggyDeque.addLast(addNumber);
                     correctDeque.addLast(addNumber);
+
+                    operation = "addLast(" + addNumber + ")";
+                    operationSequence.append(operation + "\n");
+
                     break;
                 case 2:  // removeFirst
                     if (!buggyDeque.isEmpty() && !correctDeque.isEmpty()) {
-                        assertEquals(correctDeque.removeFirst(), buggyDeque.removeFirst());
+
+                        operation = "removeFirst()";
+                        operationSequence.append(operation + "\n");
+
+                        assertEquals(operationSequence.toString(), correctDeque.removeFirst(),
+                                buggyDeque.removeFirst());
                     }
                     break;
                 case 3:  // removeLast
                     if (!buggyDeque.isEmpty() && !correctDeque.isEmpty()) {
-                        assertEquals(correctDeque.removeLast(), buggyDeque.removeLast());
+                        operation = "removeLast()";
+                        operationSequence.append(operation + "\n");
+                        assertEquals(operationSequence.toString(), correctDeque.removeLast(),
+                                buggyDeque.removeLast());
                     }
                     break;
             }

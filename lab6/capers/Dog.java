22ab3100fa60dbf51b2926d13e1d0aa3896209b3
CrispyCandy <crispycandy750@gmail.com>
2024-04-07 14:32:40 +0800

    remove extra TODO and fix the bug in Dog.fromFile()

--- lab6/capers/Dog.java
+++ lab6/capers/Dog.java
@@ -1,13 +1,14 @@
 package capers;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.Serializable;
 import static capers.Utils.*;
 
 /** Represents a dog that can be serialized.
- * @author TODO
+ * @author CrispyCandy
 */
-public class Dog implements Serializable{ // TODO
+public class Dog implements Serializable{
 
     /** Folder that dogs live in. */
     static final File DOG_FOLDER = Utils.join(CapersRepository.CAPERS_FOLDER, "dogs");
@@ -38,7 +39,7 @@ public class Dog implements Serializable{ // TODO
      * @return Dog read from file
      */
     public static Dog fromFile(String name) {
-        File dogFile = join(DOG_FOLDER, "name");
+        File dogFile = join(DOG_FOLDER, name);
         if (!dogFile.exists()) {
             return null;
         }

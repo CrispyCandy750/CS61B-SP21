d14fdf966ce432089971c35b754069cb5eb07de5
CrispyCandy <crispycandy750@gmail.com>
2024-04-10 10:49:19 +0800

    creates the .gitignore file

---
+++ proj2/gitlet/git/Commit.java
@@ -0,0 +1,74 @@
+package gitlet;
+
+// TODO: any imports you need here
+
+import java.io.File;
+import java.io.Serializable;
+import java.sql.Timestamp;
+import java.util.Date; // TODO: You'll likely use this in this class
+import java.util.Map;
+
+/** Represents a gitlet commit object.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ *  @author TODO
+ */
+/** Represents a gitlet commit object and manipulate all commits objects. */
+public class Commit implements Serializable {
+    /**
+     * TODO: add instance variables here.
+     *
+     * List all instance variables of the Commit class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided one example for `message`.
+     */
+
+    private final static File COMMITS_DIR = Utils.join(GitRepo.GIT_REPO, "objects");
+
+    /** The message of this Commit. */
+    private String message;
+    private Timestamp timestamp;
+    private String parent;
+    private Map<File, Blob> files;
+
+    public Commit(String message, String parent, Map<File, Blob> files) {
+        new Commit(message, parent, System.currentTimeMillis(), files);
+    }
+
+    private Commit(String message, String parent, long time, Map<File, Blob> files) {
+        this.message = message;
+        this.parent = parent;
+        this.timestamp = new Timestamp(time);
+        this.files = files;
+    }
+
+    /* TODO: fill in the rest of this class. */
+
+    /** Create the .gitlet/objects/ directory and initial commit. */
+    public static void init() {
+        COMMITS_DIR.mkdir();
+        Commit initialCommit = initialCommit();
+        initialCommit.saveCommit();
+    }
+
+    /** Creates the initial commit. */
+    private static Commit initialCommit() {
+        return new Commit("initial message", null, 0, null);
+    }
+
+    /** Save the commit in the objects directory with the name as its hashcode. */
+    private void saveCommit() {
+        String commitId = Utils.sha1(this);
+
+        /* Creates the directory contains this commit. */
+        File commitDir = Utils.join(COMMITS_DIR, commitId.substring(0, 2));
+        if (!commitDir.exists()) {
+            commitDir.mkdir();
+        }
+
+        /* Save the commit. */
+        File commitFile = Utils.join(commitDir, commitId.substring(2));
+        Utils.writeObject(commitFile, this);
+    }
+}
---
+++ proj2/gitlet/git/Repository.java
@@ -0,0 +1,47 @@
+package gitlet;
+
+import java.io.File;
+
+import static gitlet.Utils.*;
+
+// TODO: any imports you need here
+
+/**
+ * Represents a gitlet repository.
+ *  TODO: It's a good idea to give a description here of what else this Class
+ *  does at a high level.
+ *
+ * @author TODO
+ */
+public class Repository {
+    /**
+     * TODO: add instance variables here.
+     *
+     * List all instance variables of the Repository class here with a useful
+     * comment above them describing what that variable represents and how that
+     * variable is used. We've provided two examples for you.
+     */
+
+    /** The current working directory. */
+    public static final File CWD = new File(System.getProperty("user.dir"));
+
+    /**
+     * Creates a new Gitlet version-control system in the current directory.
+     * This system will automatically start with one commit: a commit that contains no files and
+     * has the commit message initial commit (just like that, with no punctuation).
+     * It will have a single branch: master, which initially points to this initial commit, and
+     * master will be the current branch.
+     * The timestamp for this initial commit will be 00:00:00 UTC, Thursday, 1 January 1970 in
+     * whatever format you choose for dates (this is called “The (Unix) Epoch”, represented internally
+     * by the time 0.)
+     * Since the initial commit in all repositories created by Gitlet will have exactly the same content,
+     * it follows that all repositories will automatically share this commit (they will all have the same UID)
+     * and all commits in all repositories will trace back to it.
+     */
+    public static void init() {
+        File gitletDir = join(CWD, ".gitlet");
+        if (gitletDir.exists()) {
+            System.out.println("the ");
+        }
+    }
+}

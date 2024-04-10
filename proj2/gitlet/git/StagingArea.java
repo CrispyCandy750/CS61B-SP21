994b3e4cc5d263c0c103f74f3f5da300215ed714
CrispyCandy <crispycandy750@gmail.com>
2024-04-10 16:22:15 +0800

    reconstruct the StagingArea class

--- proj2/gitlet/git/StagingArea.java
+++ proj2/gitlet/git/StagingArea.java
@@ -1,20 +1,56 @@
 package gitlet.git;
 
 import java.io.File;
-import java.io.IOException;
+import java.io.Serializable;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 /** Represents the Staging Area. */
-public class StagingArea {
+public class StagingArea implements Serializable {
 
     /** Represents the .gitlet/index file. */
     public final static File INDEX_FILE = Utils.join(GitRepo.GIT_REPO, "index");
 
+    /** Represents the pointer from files to blob */
+    private Map<String, String> stagedFiles;
+    private Set<String> removedFiles;
+
+
     /** Initialization: Create the .gitlet/index file. */
     public static void init() {
-        try {
-            INDEX_FILE.createNewFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        StagingArea stagingArea = new StagingArea();
+        stagingArea.save();
+    }
+
+    /** Add the pointer from filename to blob. */
+    public static void add(String fileName, String blobId) {
+        StagingArea stagingArea = getStagingArea();
+        stagingArea.addToStagingArea(fileName, blobId);
+        stagingArea.save();
+    }
+
+    /* ---------------------------- private class & methods ---------------------------- */
+
+    private StagingArea() {
+        stagedFiles = new HashMap<>();
+        removedFiles = new HashSet<>();
+    }
+
+    private static StagingArea getStagingArea() {
+        return Utils.readObject(INDEX_FILE, StagingArea.class);
+    }
+
+    /* ----------------------------------- instance methods ----------------------------------- */
+
+    /** Save the StagedFiles and removedFiles. */
+    private void save() {
+        Utils.writeObject(INDEX_FILE, this);
+    }
+
+    /** Add the pointer from filename to blob. */
+    private void addToStagingArea(String fileName, String blobId) {
+        this.stagedFiles.put(fileName, blobId);
     }
 }

544d284a2f4fded46c6648519faba29d48bbef7b
CrispyCandy <crispycandy750@gmail.com>
2024-04-10 16:39:12 +0800

    reconstruct the StagingArea and re-implements the init() method

--- proj2/gitlet/git/StagingArea.java
+++ proj2/gitlet/git/StagingArea.java
@@ -8,49 +8,50 @@ import java.util.Map;
 import java.util.Set;
 
 /** Represents the Staging Area. */
-public class StagingArea implements Serializable {
+public class StagingArea{
 
     /** Represents the .gitlet/index file. */
     public final static File INDEX_FILE = Utils.join(GitRepo.GIT_REPO, "index");
 
-    /** Represents the pointer from files to blob */
-    private Map<String, String> stagedFiles;
-    private Set<String> removedFiles;
-
 
     /** Initialization: Create the .gitlet/index file. */
     public static void init() {
-        StagingArea stagingArea = new StagingArea();
-        stagingArea.save();
+        StagedAndRemovedArea stagedAndRemovedArea = new StagedAndRemovedArea();
+        stagedAndRemovedArea.save();
     }
 
     /** Add the pointer from filename to blob. */
     public static void add(String fileName, String blobId) {
-        StagingArea stagingArea = getStagingArea();
-        stagingArea.addToStagingArea(fileName, blobId);
+        StagedAndRemovedArea stagingArea = getStagingArea();
+        stagingArea.add(fileName, blobId);
         stagingArea.save();
     }
 
     /* ---------------------------- private class & methods ---------------------------- */
 
-    private StagingArea() {
-        stagedFiles = new HashMap<>();
-        removedFiles = new HashSet<>();
+    private static class StagedAndRemovedArea implements Serializable {
+        /** Represents the pointer from files to blob */
+        private Map<String, String> filesToAdd;
+        private Set<String> filesToRemove;
+
+        StagedAndRemovedArea() {
+            filesToAdd = new HashMap<>();
+            filesToRemove = new HashSet<>();
+        }
+
+        /** Save the StagedFiles and removedFiles. */
+        void save() {
+            Utils.writeObject(INDEX_FILE, this);
+        }
+
+        /** Add the pointer from filename to blob. */
+        void add(String fileName, String blobId) {
+            filesToAdd.put(fileName, blobId);
+        }
     }
 
-    private static StagingArea getStagingArea() {
-        return Utils.readObject(INDEX_FILE, StagingArea.class);
-    }
-
-    /* ----------------------------------- instance methods ----------------------------------- */
-
-    /** Save the StagedFiles and removedFiles. */
-    private void save() {
-        Utils.writeObject(INDEX_FILE, this);
-    }
-
-    /** Add the pointer from filename to blob. */
-    private void addToStagingArea(String fileName, String blobId) {
-        this.stagedFiles.put(fileName, blobId);
+    /** Returns the stagedArea. */
+    private static StagedAndRemovedArea getStagingArea() {
+        return Utils.readObject(INDEX_FILE, StagedAndRemovedArea.class);
     }
 }

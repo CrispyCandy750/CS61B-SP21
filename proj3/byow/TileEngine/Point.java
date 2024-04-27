c2f25423d479eab6c804cc706dc27cdea5bc9f8d
CrispyCandy <crispycandy750@gmail.com>
2024-04-27 16:39:21 +0800

    create the Point class to represent the coordinate

---
+++ proj3/byow/TileEngine/Point.java
@@ -0,0 +1,19 @@
+package byow.TileEngine;
+
+/**
+ * Represents the Coordinates.
+ */
+public class Point {
+    public final int x;
+    public final int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+}

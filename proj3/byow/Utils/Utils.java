208735f599d0191c2fa1f247014d18f6a2157932
CrispyCandy <crispycandy750@gmail.com>
2024-04-27 16:45:43 +0800

    create the Utils class

---
+++ proj3/byow/Utils/Utils.java
@@ -0,0 +1,14 @@
+package byow.Utils;
+
+public class Utils {
+    /** Returns the max value of the ints. */
+    public static int getMax(int[] ints) {
+        int max = ints[0];
+        for (int i = 1; i < ints.length; i++) {
+            if (ints[i] > max) {
+                max = ints[i];
+            }
+        }
+        return max;
+    }
+}
